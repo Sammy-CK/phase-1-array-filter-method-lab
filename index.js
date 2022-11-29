6d99ca6e46645af4550e8a49a96faffa4cb6b83b
Sammy-CK <sammycherono2696@gmail.com>
2022-11-29 14:40:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    const matching = drivers.filter(item => item.toUpperCase() === name.toUpperCase())
+    return matching;
+}
+
+
+function fuzzyMatch(drivers,letter){
+const count = letter.length;
+const matchStart =  drivers.filter(item => item.substring(0,count) === letter);
+console.log(matchStart)
+return matchStart;
+}
+
+
+function matchName(driver, test){
+const nameSimilar = driver.filter(item => item.name === test);
+return nameSimilar;
+}
